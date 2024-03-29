2d79b37eace3d78d6b5a1a9bc932c88edb1ce9bf
JupiterPi <jupiterpi@outlook.de>
2024-03-29 02:34:06 +0100

    test change v2.2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const http = require("http");
 
-console.log("Hello there from v2.1!");
+console.log("Hello there from v2.2!");
 
 const port = process.env["PORT"];
 
 http.createServer((req, res) => {
-    res.write(`Hello there v2.1 (called on ${req.url})`);
+    res.write(`Hello there v2.2 (called on ${req.url})`);
     res.end();
 }).listen(port, () => console.log(`Started on port ${port}`));
